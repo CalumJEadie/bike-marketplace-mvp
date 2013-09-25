a2ae5ba53372e090281647b0d439ecbe9d52e0ad
Calum J. Eadie <calum@calumjeadie.com>
2013-09-25 13:27:19 +0100

    Created basic view

--- bike_marketplace_mvp/urls.py
+++ bike_marketplace_mvp/urls.py
@@ -6,13 +6,16 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+import views
+
 
 urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
 
-    url(r"^account/", include("account.urls")),
+    # e.g. /bikes/1/
+    url(r'^bikes/(?P<bike_id>\d+)/$', views.detail, name='detail'),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
