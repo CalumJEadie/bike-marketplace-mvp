cf4912a734fe105275e64b67a10460e534b9078a
Calum J. Eadie <calum@calumjeadie.com>
2013-09-27 14:04:34 +0100

    Added carousel

--- bike_marketplace_mvp/views.py
+++ bike_marketplace_mvp/views.py
@@ -4,7 +4,20 @@ from models import Bike
 
 def detail(request, bike_id):
     bike = get_object_or_404(Bike, pk=bike_id)
-    return render(request, 'detail.html', {'bike': bike})
+    picture_ids = range(1,3)
+
+    picture_ids = {
+        1: [1],
+        2: [1],
+        3: range(1,9),
+        4: range(1,7),
+        5: range(1,10),
+        6: range(1,9),
+        7: range(1,9),
+        8: range(1,9),
+    }
+
+    return render(request, 'detail.html', {'bike': bike, 'picture_ids': picture_ids[bike.id]})
 
 def search(request):
     bikes = Bike.objects.all()
